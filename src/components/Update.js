eb0414420fbfc24304c1f6da107e765296b27d40
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-08-11 00:29:58 +0600

    Successfully CRUD Operations

--- src/components/Update.js
+++ src/components/Update.js
@@ -8,10 +8,25 @@ const Update = () => {
      const handleUpdate =event =>{
         event.preventDefault();
         const form = event.target;
-        const updateName = form.name.value;
-        const updateEmail = form.email.value;
-        const updateUser = {updateName, updateEmail}
+        const name = form.name.value;
+        const email = form.email.value;
+        const updateUser = {name, email}
         console.log(updateUser);
+
+        fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+            method:'PUT',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(updateUser)
+        })
+        .then(res =>res.json())
+        .then(data =>{
+            console.log(data);
+           if(data.modifiedCount> 0) {
+            alert('Update Successfully')
+           }
+        })
 
      }
 
